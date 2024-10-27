dc56b0550efbfc7e0ab448c312ab2d52dc5810b9
Yesica  Romina Romero <yesica.romi.romero@gmail.com>
2024-10-27 18:46:22 -0300

    primer comit

---
+++ TP_VerdeEnsaladas/src/ar/edu/unlam/pb1/parcial/dominio/Ingrediente.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.pb1.parcial.dominio;
+
+public class Ingrediente {
+	
+	private String nombre;
+	private double precio;
+	private Origen origen;
+
+}
---
+++ TP_VerdeEnsaladas/src/ar/edu/unlam/pb1/parcial/dominio/Pedido.java
@@ -0,0 +1,21 @@
+package ar.edu.unlam.pb1.parcial.dominio;
+
+public class Pedido {
+	private String nombreDelCliente;
+	private Ensalada[] ensaladas;
+	
+	public boolean agregarNuevaEnsalada(Ensalada nueva) {
+		/*
+		 * Se incorpora una nueva ensalada a la lista del pedido actual
+		 */
+		return false;
+	}
+	
+	public double getImporte() {
+		/*
+		 * Calcula el importe en función del precio de cada ingrediente
+		 */
+		return 0.0;
+	}
+}
+
